23265d630e81d3e96aa122ca2302d75dc3ed1430
Paleksz <96802620+Hardyzom@users.noreply.github.com>
2022-03-16 11:21:50 +0100

    Add files via upload

---
+++ marc16nyolcadik.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "prog1.h"
+#include <stdlib.h>
+
+int main(){
+
+string s = "10";
+int x = atoi(s);
+printf("%d\n",x);
+
+string ss = "3.14";
+double y = atof(ss);
+printf("%lf\n",y);
+
+return 0;
+
+}
